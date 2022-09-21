6b27b3c8b37f27c72512f0934bb6a922f8abed5c
Anh <trantuananh291299@gmail.com>
2022-09-22 00:28:11 +0700

    first commit

--- app/src/main/java/com/tunanh/gapotest/model/Post.kt
+++ app/src/main/java/com/tunanh/gapotest/model/Post.kt
@@ -4,6 +4,30 @@ import com.google.gson.annotations.SerializedName
 
 data class Post(
     @SerializedName("data")
-    val data: List<Data>? = null
+    val data: List<Data>? = null,
 )
-data class Data()
+
+data class Data(
+    @SerializedName("content")
+    val title: String,
+    @SerializedName("content")
+    val createTime: Long,
+    @SerializedName("post_type")
+    val postType: Long,
+    @SerializedName("mediaData")
+    val mediaData: List<Metadata>,
+    val user: User,
+
+)
+
+data class Metadata(
+    @SerializedName("src")
+    val image: String,
+)
+
+data class User(
+    @SerializedName("display_name")
+    val name: String,
+    val avatar: String,
+)
+
