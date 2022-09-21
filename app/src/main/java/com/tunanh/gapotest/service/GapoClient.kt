8170fbb2ad5dd03f53ea5243c6181cd919228a24
Anh <trantuananh291299@gmail.com>
2022-09-22 00:28:52 +0700

    first commit

--- app/src/main/java/com/tunanh/gapotest/service/GapoClient.kt
+++ app/src/main/java/com/tunanh/gapotest/service/GapoClient.kt
@@ -1,9 +1,9 @@
 package com.tunanh.gapotest.service
 
-import com.tunanh.gapotest.model.ItemPost
+import com.tunanh.gapotest.model.Post
 import retrofit2.http.GET
 
 interface GapoClient {
     @GET("feed")
-    suspend fun getAllPost():List<ItemPost>
+    suspend fun getAllPost():List<Post>
 }
